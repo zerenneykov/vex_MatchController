295ce4268a4ff8112ae03c0f68d357a6a25d94e1
Zeren Neykov <145160280+zerenneykov@users.noreply.github.com>
2023-10-22 10:08:12 -0400

    Update serial_control.py

--- serial_control.py
+++ serial_control.py
@@ -4,7 +4,4 @@ import time
 arduino = serial.Serial(port='COM3', baudrate=9600)
 time.sleep(2)
 def wrtToArduino(x):
-    arduino.write(bytearray(x, 'ascii'))
-
-wrtToArduino('A')
-    
+    arduino.write(bytearray(x, 'ascii')
